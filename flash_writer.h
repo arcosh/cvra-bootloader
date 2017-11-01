880db3bd93450a325f9c5622c1fa8018bb7322fc
Matthias <mail@matthiasbock.net>
2017-11-01 19:44:12 +0100

    flash_writer.h: Added documentation

--- flash_writer.h
+++ flash_writer.h
@@ -8,16 +8,37 @@
 extern "C" {
 #endif
 
-/** Unlocks the flash for programming. */
+/**
+ * Unlocks the flash for programming
+ *
+ * In order to prevent accidental flash operations,
+ * due to e.g. electrical disturbances,
+ * the flash memory must be unlocked
+ * using magic bytes before it can be written.
+ *
+ */
 void flash_writer_unlock(void);
 
-/** Locks the flash */
+/**
+ * Locks the flash thus disabling
+ * all further flash write operations
+ */
 void flash_writer_lock(void);
 
-/** Erases the flash page at given address. */
+/**
+ * Erase flash page at the given address
+ *
+ * @param page      Pointer to any address within the flash memory sector, that shall be erased.
+ */
 void flash_writer_page_erase(void *page);
 
-/** Writes data to given location in flash. */
+/**
+ * Writes data to given location in flash
+ *
+ * @param page      Pointer i.e. target flash address to write to
+ * @param data      Pointer to data buffer to write
+ * @param len       Number of bytes to write to flash
+ */
 void flash_writer_page_write(void *page, void *data, size_t len);
 
 #ifdef __cplusplus
