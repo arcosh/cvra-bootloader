d05c0af8199e3bc90b4fb239268d122181e8e45a
Matthias <mail@matthiasbock.net>
2017-11-03 11:39:42 +0100

    verbosity.mk: Enabled COLOR, NO_COLOR, VERBOSE and QUIET make argument parsing

--- platform/verbosity.mk
+++ platform/verbosity.mk
@@ -7,21 +7,52 @@
 # This file mainly exports the symbols PRINT and Q.
 #
 
+# With COLOR and NO_COLOR the console output can be colored.
+ifdef COLOR
+ifeq ($(COLOR),0)
+NO_COLOR = 1
+endif
+ifeq ($(COLOR),no)
+NO_COLOR = 1
+endif
+ifeq ($(COLOR),off)
+NO_COLOR = 1
+endif
+endif
+
+ifndef NO_COLOR
 ANSI_RED = \033[1;31m
 ANSI_RESET = \033[0m
 PRINT = @printf "$(ANSI_RED)%s$(ANSI_RESET)\n"
-
-# e.g. make VERBOSE=1 generates additional output
-ifdef VERBOSE
-QUIET = 0
+else
+PRINT = @printf "%s\n"
 endif
 
-# e.g. make QUIET=0 generates additional output
+# With VERBOSE the QUIET option (below) is being disabled.
+ifdef VERBOSE
+ifeq ($(VERBOSE),1)
+QUIET = 0
+endif
+ifeq ($(VERBOSE),yes)
+QUIET = 0
+endif
+ifeq ($(VERBOSE),on)
+QUIET = 0
+endif
+endif
+
+# With QUIET the invoked commands are not shown in the output.
 ifndef QUIET
 QUIET = 1
 endif
 
-# If QUIET != 1, then the invoked commands are shown in the output.
+Q :=
 ifeq ($(QUIET),1)
-Q = @
+Q := @
+endif
+ifeq ($(QUIET),yes)
+Q := @
+endif
+ifeq ($(QUIET),on)
+Q := @
 endif
