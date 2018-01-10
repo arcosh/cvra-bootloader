f8cd5bc88cf433becd4e119b1404177514c91bad
Matthias Bock <mail@matthiasbock.net>
2018-01-10 11:33:15 +0100

    python-client/socketcan.py: Implemented received CAN frame queueing

--- client/can/adapters/socketcan.py
+++ client/can/adapters/socketcan.py
@@ -4,6 +4,8 @@ import socket
 import struct
 import errno
 from sys import exit
+from queue import Queue
+import threading
 
 
 class SocketCANInterface:
@@ -25,11 +27,35 @@ class SocketCANInterface:
                                     socket.CAN_RAW)
 
         self.socket.bind((interface,))
+
         # Set transmission and reception timeout
-        self.socket.settimeout(1.)
+        self.socket.settimeout(2.)
+
         # Set buffer sizes
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 4096)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 4096)
+
+        # Create queue for received CAN frames
+        self.rx_queue = Queue()
+
+        # Start independent thread for frame reception
+        t = threading.Thread(target=self.loop)
+        t.daemon = True
+        t.start()
+
+
+    def loop(self):
+        while True:
+            try:
+                frame, _ = self.socket.recvfrom(self.CAN_FRAME_SIZE)
+            except socket.timeout:
+                continue
+
+            can_id, can_dlc, data = struct.unpack(self.CAN_FRAME_FMT, frame)
+            frame = can.Frame(id=can_id, data=data[:can_dlc])
+            self.rx_queue.put(frame)
+            can.logging.debug("Received CAN frame from socket.")
+
 
     def send_frame(self, frame):
         can.logging.debug("Transmitting CAN frame via socket...")
@@ -51,12 +77,10 @@ class SocketCANInterface:
             else:
                 raise e
 
+
     def receive_frame(self):
         try:
-            frame, _ = self.socket.recvfrom(self.CAN_FRAME_SIZE)
-        except socket.timeout:
-            can.logging.debug("Socket reception timed out")
+            # Try to retrieve a frame from the queue within specified timeout period
+            return self.rx_queue.get(block=True, timeout=3)
+        except:
             return None
-        can_id, can_dlc, data = struct.unpack(self.CAN_FRAME_FMT, frame)
-        can.logging.debug("Received CAN frame from socket.")
-        return can.Frame(id=can_id, data=data[:can_dlc])
