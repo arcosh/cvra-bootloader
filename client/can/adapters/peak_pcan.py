fa875bc28f21eb087f09b4ad1b333b03c52fb761
Matthias Bock <mail@matthiasbock.net>
2018-01-14 14:18:42 +0100

    python-client/peak_pcan.py: Implemented CAN frame queueing
    * Implemented a standalone thread receiving CAN frames into a reception queue
    * Implemented controlled exit if PCANBasic library can't be loaded

--- client/can/adapters/peak_pcan.py
+++ client/can/adapters/peak_pcan.py
@@ -1,8 +1,17 @@
 
 import can
-from .PCAN_Basic.Include.PCANBasic import *
+from queue import Queue
+import threading
+from sys import exit
 
-from time import sleep
+try:
+    from .PCAN_Basic.Include.PCANBasic import *
+except ImportError:
+    can.logging.critical("Unable to load PCANBasic library.")
+    exit(1)
+
+
+
 
 class PeakPCANInterface:
     """
@@ -16,9 +25,41 @@ class PeakPCANInterface:
         result = self.pcan.Initialize(self.channel, baudrate)
         if result != PCAN_ERROR_OK:
             can.logging.error("Failed to initialize PCAN adapter.")
+
         # result = self.pcan.FilterMessages(self.channel, 0x000, 0x123, PCAN_MODE_STANDARD)
         # if result != PCAN_ERROR_OK:
         #    can.logging.warning("Failed to configure frame filtering on PCAN adapter.")
+
+        # Create queue for received CAN frames
+        self.rx_queue = Queue()
+
+        # Start independent thread for frame reception
+        self.t = threading.Thread(target=self.loop)
+        self.t.daemon = True
+        self.t.start()
+
+
+    def loop(self):
+        while True:
+            result = self.pcan.Read(self.channel)
+
+            # Nothing received
+            if result[0] != PCAN_ERROR_OK:
+                continue
+
+            # Get TPCANMsg
+            msg = result[1]
+
+            # PCAN_MESSAGE_STATUS
+            if msg.MSGTYPE == 0x80:
+                continue
+
+            # [PCAN_MESSAGE_STANDARD, PCAN_MESSAGE_RTR, PCAN_MESSAGE_EXTENDED]
+            if msg.MSGTYPE in [0x00, 0x01, 0x02]:
+                frame = can.Frame(id=msg.ID, data=bytearray(msg.DATA), data_length=msg.LEN, extended=(msg.MSGTYPE == PCAN_MESSAGE_EXTENDED))
+                self.rx_queue.put(frame)
+                can.logging.debug("Received CAN frame via PCAN adapter.")
+
 
     def send_frame(self, frame):
         can.logging.debug("Transmitting CAN frame via PCAN adapter...")
@@ -33,26 +74,10 @@ class PeakPCANInterface:
             msg.DATA[i] = int(frame.data[i])
         self.pcan.Write(self.channel, msg)
 
+
     def receive_frame(self):
-        result = self.pcan.Read(self.channel)
-
-        timeout = 10
-        while (result[0] != PCAN_ERROR_OK and timeout > 0):
-            sleep(0.1)
-            timeout -= 1
-            result = self.pcan.Read(self.channel)
-
-        if result[0] != PCAN_ERROR_OK:
+        try:
+            # Try to retrieve a frame from the queue within specified timeout period
+            return self.rx_queue.get(block=True, timeout=3)
+        except:
             return None
-
-        # Get TPCANMsg
-        msg = result[1]
-
-        if msg.MSGTYPE == 0x80:    # PCAN_MESSAGE_STATUS
-            return None
-
-        if msg.MSGTYPE in [0x00, 0x01, 0x02]:    # [PCAN_MESSAGE_STANDARD, PCAN_MESSAGE_RTR, PCAN_MESSAGE_EXTENDED]
-            can.logging.debug("Received CAN frame via PCAN adapter.")
-            return can.Frame(id=msg.ID, data=bytearray(msg.DATA), data_length=msg.LEN, extended=(msg.MSGTYPE == PCAN_MESSAGE_EXTENDED))
-
-        return None
