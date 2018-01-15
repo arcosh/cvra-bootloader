9bff4862f684d991c75988d1fcbc12757aea9d68
Matthias Bock <mail@matthiasbock.net>
2018-01-15 17:50:22 +0100

    openocd.mk: Added Makefile target 'openocd'

--- platform/openocd.mk
+++ platform/openocd.mk
@@ -2,10 +2,13 @@
 # make targets for flashing with openocd
 #
 
-.PHONY: flash
+.PHONY: openocd flash reset
+
+openocd: all
+	openocd -f oocd.cfg -c "init" -c "reset halt"
+
 flash: all
 	openocd -f oocd.cfg -c "init" -c "reset halt" -c "program $(PROJNAME).elf verify reset exit"
 
-.PHONY: reset
 reset:
 	openocd -f oocd.cfg -c "init" -c "reset run" -c "shutdown"
